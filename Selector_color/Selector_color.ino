53535bda5fc0703b1211d82c1cfcc1d299b9d882
Manuel Galindo <mfg90@live.com>
2018-05-24 15:51:48 -0500

    Basic example

---
+++ Selector_color/Selector_color.ino
@@ -0,0 +1,22 @@
+#include "TCS3200.h"
+RGB color = {0, 0, 0};
+
+void setup() {
+  Serial.println("TCS3200 Selector de color");
+  pinMode(LED_BUILTIN, OUTPUT);
+  colorimetro_iniciar();
+}
+
+void loop() {
+  color = colorimetro();
+  if (!color.TO) {
+    print_color(color);
+    Serial.print("Color detectado:\t");
+    Serial.println(selector_color(color);
+  }
+  else {
+    Serial.println("Error de sensor");
+  }
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  delay(100);
+}
